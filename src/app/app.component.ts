819aadb78d1b3fbccc503e9835e8ac191cb87b84
Eric Sexton <foopis23@gmail.com>
2021-10-12 11:07:55 -0400

    got firestore working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'messaging-app';
+  items: Observable<any[]>;
+
+  constructor(firestore: Firestore) {
+    const itemCollections = collection(firestore, 'items')
+    this.items = collectionData(itemCollections)
+  }
 }
