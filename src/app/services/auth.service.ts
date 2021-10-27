e896e1795cc642e53c6387edb97d72fdaf30f962
Eric Sexton <foopis23@gmail.com>
2021-10-26 21:28:28 -0400

    setup function in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, signOut, User, UserCredential, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { authState } from 'rxfire/auth';
+import { Observable } from 'rxjs';
+import { map, first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  public async login(email: string, password: string): Promise<UserCredential> {
+    let credential = await signInWithEmailAndPassword(this.auth, email, password);
+    return credential;
+  }
+
+  public logout(): Promise<void> {
+    return signOut(this.auth)
+  }
+
+  public async register(email: string, password: string, displayName: string) {
+    const credential = await createUserWithEmailAndPassword(this.auth, email, password);
+
+    //TODO: create user data w/ user service
+
+    return credential;
+  }
+
+  public async isLoggedIn(): Promise<boolean> {
+    return authState(this.auth).pipe(
+      first(),
+      map((user) => {
+        return !!user
+      })).toPromise();
+  }
+
+  public get authState$(): Observable<User | null> {
+    return authState(this.auth);
+  }
 }
