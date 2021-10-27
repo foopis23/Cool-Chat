6d08504c798df4bb5862e9702bf9e54269ba9352
trip-flip <thomasbaugher@outlook.com>
2021-10-27 11:51:43 -0400

    Started writing tests

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { initializeApp } from '@firebase/app';
+import { environment } from 'src/environments/environment';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage())
+      ]
+    });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get user data', (done: DoneFn) => {
+    service.getUserById('eXhtvFSmcWh4yPQ6xDcm').subscribe(user => {
+      expect(user.name).toBe('DummyUser');
+      done();
+    });
   });
 });
