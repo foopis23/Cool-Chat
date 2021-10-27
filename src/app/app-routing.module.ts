3b24a63e5f3ee01c131ab9aff88857746271e295
Eric Sexton <foopis23@gmail.com>
2021-10-27 08:37:53 -0400

    removed random changes that I made for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './login/login/login.component';
-import { RegisterComponent } from './login/register/register.component';
-import { GuardedComponent } from './test/guarded/guarded.component';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'test/guarded', component: GuardedComponent, canActivate: [AuthGuard] }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
