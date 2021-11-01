1763b798131da3e3d6b38a03119ec147e9426c63
trip-flip <thomasbaugher@outlook.com>
2021-11-01 13:39:27 -0400

    Improved tests

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -12,6 +12,11 @@ import { deleteDoc } from 'firebase/firestore';
 
 describe('UserService', () => {
   let service: UserService;
+  const testUser: User = {
+    id: '',
+    name: 'TestUser',
+    status: 0
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -30,82 +35,56 @@ describe('UserService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get user data', (done: DoneFn) => {
-    const dummyUser: User = {
-      id: 'k5UXtHjyaVncib6oNATM',
-      name: 'DummyUser',
-      status: Status.OFFLINE
-    };
-
-    service.getUserById(dummyUser.id).subscribe(user => {
-      expect(user.name).toBe(dummyUser.name);
-      expect(user.status).toBe(dummyUser.status);
-      done();
-    });
-  });
-
   it('should create a new user', (done: DoneFn) => {
-    const dummyData = {
-      name: 'DummyCreatedUser',
-      status: Status.OFFLINE
-    };
-
-    service.createUser(dummyData.name).then(docRef => {
-      const userRef = docSnapshots(docRef).pipe(
+    service.createUser(testUser.name).then(docRef => {
+      const user$ = docSnapshots(docRef).pipe(
         map(data => {
           const user = data.data();
           user!.id = data.id;
           return user as User;
         })
       );
+      user$.subscribe(user => {
+        testUser.id = user.id;
+        expect(user.name).toBe(testUser.name);
+        done();
+      })
+    });
+  });
 
-      userRef.subscribe(user => {
-        expect(user.name).toBe(dummyData.name);
-        expect(user.status).toBe(dummyData.status);
+  it('should get user data', (done: DoneFn) => {
+    service.getUserById(testUser.id).subscribe(user => {
+      expect(user.name).toBe(testUser.name);
+      expect(user.status).toBe(testUser.status);
+      done();
+    });
+  });
+
+
+  it('should change the username of a user', (done: DoneFn) => {
+    testUser.name = 'NewTestUsername';
+    service.changeUsername(testUser.id, testUser.name).then(_ => {
+      service.getUserById(testUser.id).subscribe(user => {
+        expect(user.name).toBe(testUser.name);
         done();
       });
     });
   });
 
-  it('should change the username of a user', (done: DoneFn) => {
-    const dummyUser = {
-      id: '5BOjTtwZqAv85X220ebH',
-      name: 'ToChangedDummyUser',
-      status: Status.OFFLINE
-    };
-
-    service.changeUsername(dummyUser.id, dummyUser.name);
-    service.getUserById(dummyUser.id).subscribe(user => {
-      expect(user.name).toBe(dummyUser.name);
-      expect(user.status).toBe(dummyUser.status);
-      done();
-    });
-  });
-
   it('should change the status of a user', (done: DoneFn) => {
-    const dummyUser = {
-      id: 'JDXybYhH1npHWAZOPdj5',
-      name: 'StatusDummyUser',
-      status: Status.ONLINE
-    };
-
-    service.deleteUser('CPyfcBLTC5luFqdtXnQA').then(_ => {
-      done();
-    })
-
-    service.changeStatus('JDXybYhH1npHWAZOPdj5', dummyUser.status);
-    service.changeUsername(dummyUser.id, dummyUser.name);
-    service.getUserById(dummyUser.id).subscribe(user => {
-      expect(user.name).toBe(dummyUser.name);
-      expect(user.status).toBe(dummyUser.status);
-      done();
+    testUser.status = Status.ONLINE;
+    service.changeStatus(testUser.id, testUser.status).then(_ => {
+      service.getUserById(testUser.id).subscribe(user => {
+        expect(user.status).toBe(testUser.status);
+        done();
+      });
     });
   });
 
   it('should delete a user', (done: DoneFn) => {
-    const id = '2q0k0o58WIUOLHPswM67';
-    service.deleteUser(id).then(_ => {
+    service.deleteUser(testUser.id).then(_ => {
       done();
     });
   });
+
 });
