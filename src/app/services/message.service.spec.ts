fc8626bb5f51c8911b23f48f59cb41aa5c2bda4a
Eric Sexton <foopis23@gmail.com>
2021-11-01 20:07:16 -0400

    fixed spec file

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -1,4 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { initializeApp } from '@firebase/app';
+import { environment } from 'src/environments/environment';0
 
 import { MessageService } from './message.service';
 
@@ -6,7 +12,14 @@ describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideStorage(() => getStorage())
+      ]
+    });
     service = TestBed.inject(MessageService);
   });
 
