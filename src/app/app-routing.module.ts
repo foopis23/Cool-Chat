58e5fb8cc41f6e5408e12067d2d1a6b23004b995
Eric Sexton <foopis23@gmail.com>
2021-11-06 12:24:09 -0400

    hook up views to router and pluggined in chatroom ponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
