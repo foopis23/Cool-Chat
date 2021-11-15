82ea63d4bb04246831f6ce37ac00c4fa0b57c964
Matthew <matthew.s.purdy@gmail.com>
2021-11-14 19:21:57 -0500

    Did formatting changes for messages being on either left or right side. Still need to resolve some issues (i.e. problems with current user, many messages that were entered with the message input not having an id within Firebase so they don't have a author listed, new user icon, etc.)

--- src/app/home/chatroom/chatroom.component.ts
+++ src/app/home/chatroom/chatroom.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { user } from 'rxfire/auth';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 import { MessageService, Message } from 'src/app/services/message.service';
+import { User } from 'src/app/types/User'
+import { UserQueryService } from 'src/app/services/user-query.service';
 
 @Component({
   selector: 'app-chatroom',
@@ -15,8 +19,14 @@ export class ChatroomComponent implements OnInit {
   messageInput : string;
   newUserMessage = true;
   messages : Message[];
+  //Might need to work on typing for this
+  userData: Observable<User> | undefined;
+  currentUserData$: any;
+  currentUser: User | undefined;
 
-  constructor(private messageSvc : MessageService) {
+
+
+  constructor(private messageSvc : MessageService, private authSvc: AuthService) {
     this.messages = [];
     this.messageInput = '';
   }
@@ -28,12 +38,41 @@ export class ChatroomComponent implements OnInit {
     })
 
         //TODO: subscribe to current user data here and store in variable...
+        //I used auth service for now cause that seemed like getUser would be the way to get the current logged in user
+        //But it doesn't seem to be working and using authstate didnt either so I'm not sure if im in doing  it correctly or not
+        this.currentUserData$ = this.authSvc.getUser();
+        
+        this.currentUserData$.subscribe((currentUser: User | undefined) => {
+          this.currentUser = currentUser;
+        })
+
   }
 
   isFromCurrentUser(message : Message) {
     //TODO: compare current user variable to message fromId
+    //Not really sure if this is the correct way to implement current User or not
+    /*
+    console.log(this.currentUser?.id);
+    console.log(message.from.id);
+    if (this.currentUser?.id == message.from.id) {
+      return true;
+    } else {
+      return false;
+    }
+    */
 
-    return false;
+    //Left this manual test in place for now since I don't think my current user is working right and i'm 
+    //just easily testing to see what messages look like on both sides of the screen
+    //There is also some wierdness going on regarding any of the messages that have an idea which I believe is not
+    //in the within the users (so I think thats way profile circle, username, etc. are acting up)
+    console.log("JDXybYhH1npHWAZOPdj5");
+    console.log(message.from.id);
+    console.log("JDXybYhH1npHWAZOPdj5" == message.from.id);
+    if ("JDXybYhH1npHWAZOPdj5" == message.from.id) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   submitMessage() {
--- src/app/home/message/message.component.ts
+++ src/app/home/message/message.component.ts
@@ -22,6 +22,8 @@ export class MessageComponent implements OnInit {
   @Input() lastMessage: Message | undefined;
   @Input() nextMessage: Message | undefined;
   @Input() isFromCurrentUser: boolean = false;
+  messageDay: String | undefined;
+  messageTime: String | undefined;
 
 
   constructor() {
@@ -29,7 +31,46 @@ export class MessageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.determineDay();
+    this.determineTime();
+  }
 
+  determineDay() {
+    if (this.newMessage == undefined) {
+      return
+    } else {
+      switch(this.newMessage.timestamp.toDate().getUTCDate()) {
+        case 0:
+          this.messageDay = "SUN";
+          break;
+        case 1:
+          this.messageDay = "MON";
+          break;
+        case 2:
+          this.messageDay = "TUE";
+          break;
+        case 3:
+          this.messageDay = "WED";
+          break;
+        case 4:
+          this.messageDay = "THUR";
+          break;
+        case 5:
+          this.messageDay = "FRI";
+          break;
+        case 6:
+          this.messageDay = "SAT";
+          break;
+      }
+    }
+  }
+
+  determineTime() {
+    if (this.newMessage == undefined) {
+      return
+    } else {
+      this.messageTime = this.newMessage.timestamp.toDate().toLocaleTimeString();
+    }
   }
 
   private isLastFromSameUser(): boolean {
