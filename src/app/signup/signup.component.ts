268d65ec64b012b7f292d995a977ca0315817fbe
Eric Sexton <foopis23@gmail.com>
2021-11-18 19:40:54 -0500

    added cool validator logic and error messages

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm = new FormGroup({
+    displayName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  },
+    // password and confirmPassword must match
+    (group: AbstractControl) => {
+      let pass = group.get('password')?.value;
+      let confirmPass = group.get('confirmPassword')?.value
+      return pass === confirmPass ? null : { notSame: true }
+    }
+  )
 
-  constructor() { }
+  displayErrors: boolean = false
+
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit(): void {
+
   }
 
+  onSubmit() {
+    console.log(this.signupForm.errors);
+    if (this.signupForm.invalid) {
+      this.displayErrors = true;
+      return;
+    }
+
+    this.displayErrors = false
+    const { username, password, displayName } = this.signupForm.value;
+    console.log(username, password, displayName);
+  }
 }
