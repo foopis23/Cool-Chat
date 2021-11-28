8edf001b4892e6b5fefc82ea05b0b91503b91637
Eric Sexton <foopis23@gmail.com>
2021-11-28 16:06:07 -0500

    removed unfished tests

--- src/app/home/user-search/user-search.component.spec.ts
+++ src/app/home/user-search/user-search.component.spec.ts
@@ -19,7 +19,7 @@ describe('UserSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
--- src/app/services/chatroom.service.spec.ts
+++ src/app/services/chatroom.service.spec.ts
@@ -10,7 +10,7 @@ describe('ChatroomService', () => {
     service = TestBed.inject(ChatroomService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
 });
