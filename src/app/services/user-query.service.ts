f75172a4bfc1d9fd41d8676ccfe176e3a2dfa1aa
Eric Sexton <foopis23@gmail.com>
2021-11-28 16:32:01 -0500

    removed broken test and made user a reply subject

--- src/app/services/user-query.service.ts
+++ src/app/services/user-query.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { collectionSnapshots, docSnapshots, Firestore } from '@angular/fire/firestore';
 import { doc, collection } from '@firebase/firestore';
-import { combineLatest, Observable } from 'rxjs';
+import { combineLatest, Observable, ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Status, User } from '../types/User';
 
@@ -10,26 +10,29 @@ import { Status, User } from '../types/User';
 })
 export class UserQueryService {
   private usersCollection;
-  private userObservables: { [key: string]: Observable<User> };
+  private userObservables: { [key: string]: ReplaySubject<User> };
 
   constructor(private fs: Firestore) {
     this.usersCollection = collection(fs, 'users');
     this.userObservables = {};
   }
 
-  public getUserById(id: string): Observable<User> {
+  public getUserById(id: string): ReplaySubject<User> {
     if (this.userObservables[id] !== undefined) {
       return this.userObservables[id];
     }
 
     const docRef = doc(this.usersCollection, id);
-    this.userObservables[id] = docSnapshots(docRef).pipe(
+    this.userObservables[id] = new ReplaySubject(1);
+    docSnapshots(docRef).pipe(
       map(data => {
         const user = data.data();
         user!.id = data.id;
         return user as User;
       })
-    );
+    ).subscribe((user) => {
+      this.userObservables[id].next(user);
+    })
 
     return this.userObservables[id];
   }
