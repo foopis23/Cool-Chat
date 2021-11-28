a689275e9b867674fcaf3e595966c0e554827e32
Eric Sexton <foopis23@gmail.com>
2021-11-28 16:32:21 -0500

    added multiple user test

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { UserQueryService } from '../services/user-query.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,11 +12,24 @@ import { AuthService } from '../services/auth.service';
 })
 export class SidebarComponent implements OnInit {
   public isLoggedIn: Promise<boolean> | undefined;
+  public userName : string | undefined;
 
-  constructor(private authSvc: AuthService, private _router: Router) { }
+  constructor(private authSvc: AuthService, private usrSvc: UserQueryService, private _router: Router) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.authSvc.isLoggedIn();
+    let lastUserSub : Subscription | undefined;
+    this.authSvc.authState$.subscribe((state) => {
+      if (state?.uid) {
+        if (lastUserSub !== undefined) {
+          lastUserSub.unsubscribe()
+        }
+        
+        lastUserSub = this.usrSvc.getUserById(state.uid).subscribe((user) => {
+          this.userName = user.displayName;
+        })
+      }
+    })
   }
 
   onLogout() {
